d8ec7ea601dcc48cd282abce3d490a6fc6a7bd80
Chris Joakim <christopher.joakim@gmail.com>
2019-06-06 09:53:58 -0400

    env var for AZURE_COSMOSDB_GRAPHDB_VIEWS

--- set_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Bash shell script set/augment environment variables in the current shell.
-# Chris Joakim, Microsoft, 2019/06/06
-
-export AZURE_COSMOSDB_GRAPHDB_DBNAME=dev
-export AZURE_COSMOSDB_GRAPHDB_GRAPH=npm 
-
-env | grep AZURE_COSMOSDB_GRAPHDB
